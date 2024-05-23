0a26731e573d9ffd59754eb757929351d58d14ae
Said Ahmad Nadim <dezmant03@gmail.com>
2024-05-23 03:56:31 +0300

    Update HealthCheckController.java

--- src/main/java/com/example/adminka/controller/HealthCheckController.java
+++ src/main/java/com/example/adminka/controller/HealthCheckController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping("/health-_-")
     public String checkHealth() {
-        return "Status OK";
+        return "Status OK!";
     }
 
     @GetMapping("/")
