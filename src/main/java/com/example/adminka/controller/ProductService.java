c81c8a7787a5ba5a19aa4df4c21943cb4e2d44e7
RaicherVictor <102414295+RaicherVictor@users.noreply.github.com>
2024-09-19 23:47:03 +0300

    Update ProductService.java

--- src/main/java/com/example/adminka/controller/ProductService.java
+++ src/main/java/com/example/adminka/controller/ProductService.java
@@ -11,9 +11,9 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     private List<Product> allProducts = Arrays.asList(
-            new Product(1L, "Tablet", 2999.99),
-            new Product(2L, "Phone", 699.99),
-            new Product(3L, "Laptop", 399.99)
+            new Product(1L, "Notebook", 2999.99),
+            new Product(2L, "Xiaomi", 699.99),
+            new Product(3L, "HP", 399.99)
     );
 
     public List<Product> getAllProducts() {
